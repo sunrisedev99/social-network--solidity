0a4d87ae033135d6162cba173d9a4a07d8863d45
sunrisedev99 <sunrisedev99@gmail.com>
2024-08-08 02:11:03 +0200

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,13 +20,13 @@ class Navbar extends Component {
                     <small id="account">{this.props.account}</small>
                     </small>
                     { this.props.account
-                    ? <img
-                        className='ml-2'
-                        width='30'
-                        height='30'
-                        src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
-                    />
-                    : <span></span>
+                        ? <img
+                            className='ml-2'
+                            width='30'
+                            height='30'
+                            src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
+                        />
+                        : <span></span>
                     }
                 </li>
             </ul>
